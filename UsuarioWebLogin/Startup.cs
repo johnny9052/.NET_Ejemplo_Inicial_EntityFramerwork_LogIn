08cd68cfc40d87c2da068c2488012c5f98a96c27
johnny9052 <alexander9052@gmail.com>
2016-02-24 19:43:24 -0500

    Commit inicial

---
+++ UsuarioWebLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UsuarioWebLogin.Startup))]
+namespace UsuarioWebLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
